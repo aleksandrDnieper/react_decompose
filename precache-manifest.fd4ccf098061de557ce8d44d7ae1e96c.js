8994062e28e15ad383726ce3e564a67a2fde88cb
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-02-11 19:57:56 +0200

    Updates

---
+++ precache-manifest.fd4ccf098061de557ce8d44d7ae1e96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e5224681d3ca1c1e75d26fbd3352ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8b21b7871b9800b5c82",
+    "url": "./static/css/main.3545fa61.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b21b7871b9800b5c82",
+    "url": "./static/js/main.0b0d8f05.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
